a937996d6443466fbdc6b29d27ab2ba27467d39e
Jose Augusto <joseaugustofarsousa@gmail.com>
2025-01-31 20:02:48 -0300

    Criação do use case consultar cronograma.

---
+++ src/domain/usecases/ConsultarCronogramaUseCase.ts
@@ -0,0 +1,31 @@
+import { ICronograma } from "../entities/Cronograma";
+import { IUseCase } from "../../contracts/IUseCase";
+
+export interface IEntradaConsultarCronograma {
+    disciplinaId: string;
+}
+
+export interface ISaidaConsultarCronograma {
+    cronograma: ICronograma;
+}
+
+export class ConsultarCronogramaUseCase implements IUseCase<IEntradaConsultarCronograma, ISaidaConsultarCronograma> {
+    constructor() {
+        console.log('ConsultarCronogramaUseCase instanciado');
+    }
+
+    async perform(entrada: IEntradaConsultarCronograma): Promise<ISaidaConsultarCronograma> {
+
+        const saida: ISaidaConsultarCronograma = {
+            cronograma: {
+                disciplinaId: entrada.disciplinaId,
+                atividades: [
+                    { data: "2025-02-03", descricao: "Trabalho" },
+                    { data: "2025-02-10", descricao: "P1" }
+                ]
+            }
+        };
+
+        return saida;
+    }
+}
