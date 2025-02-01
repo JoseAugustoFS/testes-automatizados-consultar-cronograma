e28cd19e063868fb7877b92721d4d2262be8d4d6
Jose Augusto <joseaugustofarsousa@gmail.com>
2025-01-31 23:00:54 -0300

    Finalizados todos os testes e modificado controller para responder a determinadas situações.

--- src/controllers/ConsultarCronogramaController.ts
+++ src/controllers/ConsultarCronogramaController.ts
@@ -16,26 +16,31 @@ export class ConsultarCronogramaController implements IController {
 
     async handle(req: Request, resp: Response): Promise<void> {
 
-        if (!this.validar(req)) {
-            resp.status(400).json({ error: "disciplinaId é obrigatório e precisa ser um número" });
+        const resultadoValidacaoEntrada = this.validarEntrada(req);
+        if (resultadoValidacaoEntrada) {
+            resp.status(400).json({ error: resultadoValidacaoEntrada });
         } else {
 
-            const { disciplinaId } = req.params;
-
-            const dto_usecase: IEntradaConsultarCronograma = {
-                disciplinaId: parseInt(disciplinaId as string),
-            };
-
-            const resposta: ISaidaConsultarCronograma = await this.useCase.perform(dto_usecase);
-            console.log('Resposta UseCase', resposta.cronograma);
-
-            const minha_resposta = {
-                mensagem: 'ConsultarCronogramaController.metodoBasico() chamado',
-                cronograma: resposta.cronograma,
-            };
-
             try {
-                resp.status(200).json(minha_resposta).end();
+                const { disciplinaId } = req.params;
+
+                const dto_usecase: IEntradaConsultarCronograma = {
+                    disciplinaId: parseInt(disciplinaId as string),
+                };
+
+                const resposta: ISaidaConsultarCronograma = await this.useCase.perform(dto_usecase);
+                console.log('Resposta UseCase', resposta.cronograma);
+
+                if (this.validarSaida(resposta)) {
+                    resp.status(500).json({ error: 'Retorno do use case não é um cronograma' });
+                } else {
+                    const minha_resposta = {
+                        mensagem: 'ConsultarCronogramaController.metodoBasico() chamado',
+                        cronograma: resposta.cronograma,
+                    };
+                    resp.status(200).json(minha_resposta).end();
+                }
+
             } catch (error: any) {
                 resp.status(500).json({ error: error.message });
             }
@@ -43,17 +48,37 @@ export class ConsultarCronogramaController implements IController {
         }
     }
 
-    private validar(req: Request): boolean {
-        if(!req) {
-            return false
-        }
+    private validarEntrada(req: Request): string | null {
         const { disciplinaId } = req.params;
-        if(isNaN(Number(disciplinaId))){
-            return false;
+        if (!disciplinaId) {
+            return "disciplinaId é obrigatório";
         }
-        if(!Number(disciplinaId) || Number(disciplinaId) < 0 || Number(disciplinaId) % 1 !== 0) {
-            return false;
+        if (isNaN(Number(disciplinaId))) {
+            return "disciplinaId precisa ser um número";
         }
-        return true;
+        if (Number(disciplinaId) < 0) {
+            return "disciplinaId precisa ser um número positivo";
+        }
+        if (Number(disciplinaId) % 1 !== 0) {
+            return "disciplinaId precisa ser um número inteiro";
+        }
+        return null;
     }
+
+    private validarSaida(resposta: ISaidaConsultarCronograma): boolean {
+        if (isNaN(Number(resposta.cronograma.disciplinaId)) || !Array.isArray(resposta.cronograma.atividades)) {
+            return true;
+        }
+        for (let i = 0; i < resposta.cronograma.atividades.length; i++) {
+            const atividade = resposta.cronograma.atividades[i];
+            if (typeof atividade.data !== 'string') {
+                return true;
+            }
+            if (typeof atividade.descricao !== 'string') {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
--- tests/controllers/ConsultarCronogramaController.test.ts
+++ tests/controllers/ConsultarCronogramaController.test.ts
@@ -3,15 +3,45 @@ import { IUseCase } from "../../src/contracts/IUseCase";
 import { IEntradaConsultarCronograma, ISaidaConsultarCronograma } from "../../src/domain/usecases/ConsultarCronogramaUseCase";
 import { Request, Response } from "express";
 
+
 class UseCaseFake implements IUseCase<IEntradaConsultarCronograma, ISaidaConsultarCronograma> {
     chamado: boolean = false;
-    async perform(entrada: IEntradaConsultarCronograma): Promise<ISaidaConsultarCronograma> {
-        return {
-            cronograma: {
-                disciplinaId: entrada.disciplinaId,
-                atividades: [{ data: "2025-02-03", descricao: "Teste" }]
-            }
-        };
+    opcao: string;
+    constructor (opcao: string) {
+        this.opcao = opcao;
+    }
+    async perform(entrada: IEntradaConsultarCronograma): Promise<any> {
+        if(this.opcao === "certo") {
+            return {
+                cronograma: {
+                    disciplinaId: entrada.disciplinaId,
+                    atividades: [{ data: "2025-02-03", descricao: "Teste" }]
+                }
+            };
+        }if(this.opcao === "erro") {
+            throw new Error("Erro inesperado");
+        } if(this.opcao === "errado1") {
+            return {
+                cronograma: {
+                    disciplinaId: 'teste',
+                    atividades: [{ data: "2025-02-03", descricao: "Teste" }]
+                }
+            };
+        } if(this.opcao === "errado2") {
+            return {
+                cronograma: {
+                    disciplinaId: '1',
+                    atividades: [{ data: false, descricao: "Teste" }]
+                }
+            };
+        } if(this.opcao === "errado3") {
+            return {
+                cronograma: {
+                    disciplinaId: '1',
+                    atividades: [{ data: "2025-02-03", descricao: false }]
+                }
+            };
+        }
     }
 }
 
@@ -36,14 +66,14 @@ class ResponseFake {
     }
 }
 
-function makeSUT() {
+function makeSUT(disciplinaId: any, opcao: string) {
     const requestStub = {
         params: {
-            disciplinaId: '1',
+            disciplinaId: disciplinaId,
         },
     } as any as Request;
     const responseFake = new ResponseFake();
-    const uc = new UseCaseFake();
+    const uc = new UseCaseFake(opcao);
     const controller = new ConsultarCronogramaController(uc);
     return { uc, controller, requestStub, responseFake };
 }
@@ -51,12 +81,12 @@ function makeSUT() {
 describe('ControllerBasico', () => {
     
     it('deve instanciar ConsultarCronogramaController', () => {
-        let { uc, controller, requestStub, responseFake } = makeSUT();
+        let { uc, controller, requestStub, responseFake } = makeSUT('1', "certo");
         expect(controller).toBeDefined();
     });
 
     it('deve chamar handle', async () => {
-        let { uc, controller, requestStub, responseFake } = makeSUT();
+        let { uc, controller, requestStub, responseFake } = makeSUT('1', "certo");
         await controller.handle(requestStub, responseFake as any as Response);
 
         expect(responseFake.endChamado).toBe(true);
@@ -66,4 +96,86 @@ describe('ControllerBasico', () => {
         expect(responseFake.jsonInformado.cronograma.atividades[0].descricao).toBe("Teste");
         
     });
+
+    it('deve retornar mensagem de erro e código 400 caso não passe parâmetro', async () => {
+        let { uc, controller, requestStub, responseFake } = makeSUT(undefined, "certo");
+        await controller.handle(requestStub, responseFake as any as Response);
+
+        expect(responseFake.endChamado).toBe(false);
+        expect(responseFake.statusCodeInformado).toBe(400);
+        expect(responseFake.jsonInformado.error).toBe("disciplinaId é obrigatório");
+    });
+
+    it('deve retornar mensagem de erro e código 400 caso passe parâmetro null', async () => {
+        let { uc, controller, requestStub, responseFake } = makeSUT(null, "certo");
+        await controller.handle(requestStub, responseFake as any as Response);
+
+        expect(responseFake.endChamado).toBe(false);
+        expect(responseFake.statusCodeInformado).toBe(400);
+        expect(responseFake.jsonInformado.error).toBe("disciplinaId é obrigatório");
+    });
+
+    it('deve retornar mensagem de erro e código 400 caso o parâmetro não seja um número', async () => {
+        let { uc, controller, requestStub, responseFake } = makeSUT('teste', "certo");
+        await controller.handle(requestStub, responseFake as any as Response);
+
+        expect(responseFake.endChamado).toBe(false);
+        expect(responseFake.statusCodeInformado).toBe(400);
+        expect(responseFake.jsonInformado.error).toBe("disciplinaId precisa ser um número");
+    });
+
+    it('deve retornar mensagem de erro e código 400 caso o parâmetro seja um número negativo', async () => {
+        let { uc, controller, requestStub, responseFake } = makeSUT('-1', "certo");
+        await controller.handle(requestStub, responseFake as any as Response);
+
+        expect(responseFake.endChamado).toBe(false);
+        expect(responseFake.statusCodeInformado).toBe(400);
+        expect(responseFake.jsonInformado.error).toBe("disciplinaId precisa ser um número positivo");
+    });
+
+    it('deve retornar mensagem de erro e código 400 caso o parâmetro não seja um número inteiro', async () => {
+        let { uc, controller, requestStub, responseFake } = makeSUT('1.6', "certo");
+        await controller.handle(requestStub, responseFake as any as Response);
+
+        expect(responseFake.endChamado).toBe(false);
+        expect(responseFake.statusCodeInformado).toBe(400);
+        expect(responseFake.jsonInformado.error).toBe("disciplinaId precisa ser um número inteiro");
+    });
+
+    it('deve retornar mensagem de erro e código 500 caso algum erro inesperado ocorra', async () => {
+        let { uc, controller, requestStub, responseFake } = makeSUT('1', "erro");
+        await controller.handle(requestStub, responseFake as any as Response);
+
+        expect(responseFake.endChamado).toBe(false);
+        expect(responseFake.statusCodeInformado).toBe(500);
+        expect(responseFake.jsonInformado.error).toBe("Erro inesperado");
+    });
+
+    it('deve retornar mensagem de erro e código 500 caso a saída do use case não seja um cronograma', async () => {
+        let { uc, controller, requestStub, responseFake } = makeSUT('1', "errado1");
+        await controller.handle(requestStub, responseFake as any as Response);
+
+        expect(responseFake.endChamado).toBe(false);
+        expect(responseFake.statusCodeInformado).toBe(500);
+        expect(responseFake.jsonInformado.error).toBe("Retorno do use case não é um cronograma");
+    });
+
+    it('deve retornar mensagem de erro e código 500 caso a saída do use case não seja um cronograma', async () => {
+        let { uc, controller, requestStub, responseFake } = makeSUT('1', "errado2");
+        await controller.handle(requestStub, responseFake as any as Response);
+
+        expect(responseFake.endChamado).toBe(false);
+        expect(responseFake.statusCodeInformado).toBe(500);
+        expect(responseFake.jsonInformado.error).toBe("Retorno do use case não é um cronograma");
+    });
+
+    it('deve retornar mensagem de erro e código 500 caso a saída do use case não seja um cronograma', async () => {
+        let { uc, controller, requestStub, responseFake } = makeSUT('1', "errado3");
+        await controller.handle(requestStub, responseFake as any as Response);
+
+        expect(responseFake.endChamado).toBe(false);
+        expect(responseFake.statusCodeInformado).toBe(500);
+        expect(responseFake.jsonInformado.error).toBe("Retorno do use case não é um cronograma");
+    });
+
 });
