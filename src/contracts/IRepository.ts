a7b192709e1d82bc7338dc4bd9f845ca04750edc
Jose Augusto <joseaugustofarsousa@gmail.com>
2025-02-11 06:29:00 -0300

    Criação de interfaces de repositórios para diferentes funções.

--- src/contracts/IRepository.ts
+++ src/contracts/IRepository.ts
@@ -5,3 +5,20 @@ export interface IRepository<E> {
     update(id: string, obj: E): Promise<E>;
     delete(id: string): Promise<boolean>;
 }
+
+export interface IRepositoryFind<E> {
+    findAll(): Promise<E[]>;
+    findById(id: string): Promise<E | undefined>;
+}
+
+export interface IRepositoryCreate<E> {
+    create(id: string, obj: E): Promise<E>;
+}
+
+export interface IRepositoryUpdate<E> {
+    update(id: string, obj: E): Promise<E>;
+}
+
+export interface IRepositoryDelete<E> {
+    delete(id: string): Promise<boolean>;
+}
