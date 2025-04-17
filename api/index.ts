829c271075799c29e9dc1358340ddce47eb4789c
Jose Augusto <joseaugustofarsousa@gmail.com>
2025-04-17 07:33:37 -0300

    Adicionando um index.ts para redirecionar as rotas

---
+++ api/index.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from 'express'
+import { routerBasico } from '../src/infra/routes/exemplo_router'
+import { VercelRequest, VercelResponse } from '@vercel/node'
+
+const app = express()
+
+app.use(express.json())
+app.use('/', routerBasico)
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+    app(req as unknown as Request, res as unknown as Response)
+}
