da21f0956ccbbe6ab36bea846c39dd48aac7106f
Jose Augusto <joseaugustofarsousa@gmail.com>
2025-04-18 07:03:57 -0300

    Finalizando perte do vercel.

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,21 @@
-// api/index.js
-export default function handler(req: any, res: any) {
-    res.status(200).json({ msg: "Hello from API!" });
-  }
-  
+import express from 'express';
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+import { routerBasico } from '../src/infra/routes/exemplo_router';
+import { factoryConsultarCronograma } from '../src/infra/factories/ConsultarCronogramaFactory';
+
+const app = express();
+app.use(express.json());
+
+// Rota básica viva
+app.use('/', routerBasico);
+
+// Registrar rota direto aqui
+const objeto = factoryConsultarCronograma();
+app.get('/cronograma/:disciplinaId', (req, res) => {
+  objeto.handle(req, res);
+});
+
+// Handler para Vercel
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  app(req as any, res as any);
+}
