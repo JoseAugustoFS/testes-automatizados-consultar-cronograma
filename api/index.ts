b1be650771ad4465a9680eebce30d3b40f99b099
José Augusto <134980478+JoseAugustoFS@users.noreply.github.com>
2025-04-19 18:29:41 -0300

    Permitindo que o front acesse o back.

--- api/index.ts
+++ api/index.ts
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://front-testes-automatizados-consultar-cronograma.vercel.app',
   methods: ['GET', 'POST', 'OPTIONS'],
 }));
 
